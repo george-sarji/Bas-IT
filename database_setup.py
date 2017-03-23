cd49d50fbfed9b73d69aacebd395fa231caae331
george17-meet <george17@meet.mit.edu>
2017-03-23 19:15:47 +0200

    First commit, started MVP, done sign up and sign in.

--- database_setup.py
+++ database_setup.py
@@ -1,9 +1,64 @@
-from sqlalchemy import Column, Date, Float, ForeignKey, Integer, String
+from sqlalchemy import Column,Integer,String, Date, ForeignKey, Float, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
+from passlib.apps import custom_app_context as pwd_context
+import random, string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
 
 #PLACE YOUR TABLE SETUP INFORMATION HERE
 
+
+
+class User(Base):
+	__tablename__ = 'user'
+	id = Column(Integer, primary_key=True)
+	name = Column(String(255))
+	email = Column(String(255))
+	password = Column(String)
+	country = Column(String)
+	language = Column(String)
+	dob = Column(Date)
+	instructor = Column(Boolean)
+
+
+	def hash_password(self, password):
+		self.password = pwd_context.encrypt(password)
+
+	def set_photo(self, photo):
+		self.photo = photo
+
+	def verify_password(self, password):
+		return pwd_context.verify(password, self.password)
+
+class Course(Base):
+	__tablename__ = 'course'
+	id = Column(Integer, primary_key=True)
+	subject = Column(String(255))
+	lessons = Column(Integer)
+	instructor = Column(Integer, ForeignKey('user.id'))
+	attendees = Column(Integer)
+
+
+class Lesson(Base):
+	__tablename__ = 'lesson'
+	id = Column(Integer, primary_key=True)
+	course = Column(Integer, ForeignKey('course.id'))
+	date = Column(Date)
+	number = Column(Integer)
+
+
+
+class CourseAttendee(Base):
+	__tablename__='courseattendee'
+	id=Column(Integer, primary_key=True)
+	user_id = Column(Integer, ForeignKey('user.id'))
+	course_id = Column(Integer, ForeignKey('course.id'))
+
+
+engine = create_engine('sqlite:///Basit.db')
+
+
+Base.metadata.create_all(engine)
--- web_app.py
+++ web_app.py
@@ -1,28 +1,94 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, flash, render_template, redirect, request, g, send_from_directory
+from flask import session as login_session
+# from model import *
+from werkzeug.utils import secure_filename
+import locale, os
 app = Flask(__name__)
-
+app.secret_key="this is my project"
 # SQLAlchemy stuff
 ### Add your tables here!
 # For example:
 # from database_setup import Base, Potato, Monkey
-from database_setup import Base
+from database_setup import *	
 
-from sqlalchemy import create_engine
+from datetime import datetime
+
+from sqlalchemy import create_engine, desc, asc
+
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///project.db')
+# from model import *
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'static'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+engine = create_engine('sqlite:///Basit.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
-session = DBSession()
+dbsession = DBSession()
 
 
-#YOUR WEB APP CODE GOES HERE
+def allowed_file(filename):
+	return '.' in filename and \
+		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 
+def verify_login(email, password):
+	user=dbsession.query(User).filter_by(email=email).first()
+	if (user is None):
+		return False
+	elif (user.verify_password(password)):
+		return True
+	return False
 
 
+@app.route	('/')
+def home():
+	return render_template('main_page.html')
+
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+	if (request.method=='GET'):
+		return render_template('signup.html')
+	elif request.method=='POST':
+		name = request.form['name']
+		email = request.form['email']
+		password = request.form['password']
+		confirmpass = request.form['confpassword']
+		country = request.form['country']
+		dob = request.form['dob'].split('-')
+		birth = datetime(year = (int)(dob[0]), month=(int)(dob[1]), day = (int)(dob[2]))
+		if (password != confirmpass):
+			flash("Your passwords do not match.")
+			return redirect(url_for('signup'))
+		else:
+			newuser = User(email = email, name =name, country=country, dob =birth)
+			newuser.hash_password(password)
+			dbsession.add(newuser)
+			dbsession.commit()
+			flash("You have signed up successfully. Welcome aboard, "+name)
+			return redirect(url_for('home'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if (request.method=='GET'):
+		return render_template('login.html')
+	elif request.method=='POST':
+		email = request.form['email']
+		password = request.form['password']
+		if (verify_login(email, password)):
+			flash("Successfully logged in. Welcome back!")
+			return redirect(url_for('home'))
+		else:
+			flash("Wrong credentials.")
+			return redirect(url_for('login'))
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
+
